394a3cfa377b4a633baf890209a62eee01d7ec29
nathaliefc1 <nathaliefc89@gmail.com>
2023-02-19 19:59:00 +0100

    Adding Chakra for ui for style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import {useState, useEffect} from "react";
+import { Heading, Box, Image, Flex, Text, Spacer, Tag } from '@chakra-ui/react';
 import * as API from "./services/launches";
 import logo from "./assets/logo-spaceX.png";
+import { chakra } from "@chakra-ui/react";
 
 export function App() {
   const [launches, setLaunches] = useState([]);
@@ -11,15 +13,31 @@ export function App() {
 
   return (
     <>
-      <img src={logo} width={300} />
-      <h1>SpaceX Launches</h1>
-      <ul>
+      <Image m={4} src={logo} width={300} />
+      <Heading as="h1" size='lg' m={4}>SpaceX Launches</Heading>
+      <section>
         {launches.map(launch =>(
-          <li key={launch.flight_number}>
+          <Box 
+            key={launch.flight_number} 
+            bg="gray.100" 
+            p={4} 
+            m={4} 
+            borderRadius="lg"
+          >
+            <Flex display="flex">
+              <Text fontSize="2xl">
+                Mision <strong>{launch.mission_name}</strong> (
+                  {launch.launch_year})
+              </Text>
+              <Spacer/>
+              <Tag p={4} colorScheme="green">
+                {launch.launch_success ? "Success" : "Failure"}
+              </Tag>
+            </Flex>
             {launch.mission_name} ({launch.launch_year})
-          </li>
+          </Box>
         ))}
-      </ul>
+      </section>
     </>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
 import { App } from './App'
+import { ChakraProvider } from '@chakra-ui/react'
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
